c590e8161a4d47b5b484bbec5010e80370fe540e
Nithya Renganathan <narengan@us.ibm.com>
2017-03-21 06:13:12 -0500

    converted swift config file owner to variable

--- controls/check-object.rb
+++ controls/check-object.rb
@@ -1,56 +1,60 @@
-# check-swift-01
-describe file('/etc/swift/swift.conf') do
-  it { should be_owned_by 'root' }
-  its('group') { should eq 'swift' }
-end
-describe file('/etc/swift/api-paste.ini') do
-  it { should be_owned_by 'root' }
-  its('group') { should eq 'swift' }
-end
-describe file('/etc/swift/policy.json') do
-  it { should be_owned_by 'root' }
-  its('group') { should eq 'swift' }
-end
-describe file('/etc/swift/rootwrap.conf') do
-  it { should be_owned_by 'root' }
-  its('group') { should eq 'swift' }
-end
-
-# check-swift-02
-describe file('/etc/swift/swift.conf') do
-  its('mode') { should eq 0640 }
-  it { should exist }
-end
-describe file('/etc/swift/api-paste.ini') do
-  its('mode') { should eq 0640 }
-  it { should exist }
-end
-describe file('/etc/swift/policy.json') do
-  its('mode') { should eq 0640 }
-  it { should exist }
-end
-describe file('/etc/swift/rootwrap.conf') do
-  its('mode') { should eq 0640 }
-  it { should exist }
+swift_config_owner = attribute('swift_config_owner', default: 'root', description: 'OpenStack Swift config file owner')
+control 'check-swift-01' do
+  describe file('/etc/swift/swift.conf') do
+    it { should be_owned_by swift_config_owner }
+    its('group') { should eq 'swift' }
+  end
+  describe file('/etc/swift/api-paste.ini') do
+    it { should be_owned_by swift_config_owner }
+    its('group') { should eq 'swift' }
+  end
+  describe file('/etc/swift/policy.json') do
+    it { should be_owned_by swift_config_owner }
+    its('group') { should eq 'swift' }
+  end
+  describe file('/etc/swift/rootwrap.conf') do
+    it { should be_owned_by swift_config_owner }
+    its('group') { should eq 'swift' }
+  end
 end
 
-# Default listening ports for the various storage services
-describe port(6002) do
-  it { should be_listening }
-  its('protocol') {should eq 'tcp'}
+control 'check-swift-02' do
+  describe file('/etc/swift/swift.conf') do
+    its('mode') { should eq 0640 }
+    it { should exist }
+  end
+  describe file('/etc/swift/api-paste.ini') do
+    its('mode') { should eq 0640 }
+    it { should exist }
+  end
+  describe file('/etc/swift/policy.json') do
+    its('mode') { should eq 0640 }
+    it { should exist }
+  end
+  describe file('/etc/swift/rootwrap.conf') do
+    its('mode') { should eq 0640 }
+    it { should exist }
+  end
 end
 
-describe port(6001) do
-  it { should be_listening }
-  its('protocol') {should eq 'tcp'}
-end
+control 'check-swift-03' do
+  describe port(6002) do
+    it { should be_listening }
+    its('protocol') {should eq 'tcp'}
+  end
 
-describe port(6000) do
-  it { should be_listening }
-  its('protocol') {should eq 'tcp'}
-end
+  describe port(6001) do
+    it { should be_listening }
+    its('protocol') {should eq 'tcp'}
+  end
 
-describe port(873) do
-  it { should be_listening }
-  its('protocol') {should eq 'tcp'}
+  describe port(6000) do
+    it { should be_listening }
+    its('protocol') {should eq 'tcp'}
+  end
+
+  describe port(873) do
+    it { should be_listening }
+    its('protocol') {should eq 'tcp'}
+  end
 end
