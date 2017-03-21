57959816f540c0ffcb6021f9180cf4abc20911e6
Nithya Renganathan <narengan@us.ibm.com>
2017-03-21 12:02:39 -0500

    Refactored object inspec checks
    Signed-off-by: Nithya Renganathan <narengan@us.ibm.com>

--- controls/check-object.rb
+++ controls/check-object.rb
@@ -3,58 +3,56 @@ control 'check-swift-01' do
   describe file('/etc/swift/swift.conf') do
     it { should be_owned_by swift_config_owner }
     its('group') { should eq 'swift' }
-  end
-  describe file('/etc/swift/api-paste.ini') do
-    it { should be_owned_by swift_config_owner }
-    its('group') { should eq 'swift' }
-  end
-  describe file('/etc/swift/policy.json') do
-    it { should be_owned_by swift_config_owner }
-    its('group') { should eq 'swift' }
-  end
-  describe file('/etc/swift/rootwrap.conf') do
-    it { should be_owned_by swift_config_owner }
-    its('group') { should eq 'swift' }
+    its('mode') { should eq 0640 }
+    it { should exist }
   end
 end
 
 control 'check-swift-02' do
-  describe file('/etc/swift/swift.conf') do
-    its('mode') { should eq 0640 }
-    it { should exist }
-  end
   describe file('/etc/swift/api-paste.ini') do
-    its('mode') { should eq 0640 }
-    it { should exist }
-  end
-  describe file('/etc/swift/policy.json') do
-    its('mode') { should eq 0640 }
-    it { should exist }
-  end
-  describe file('/etc/swift/rootwrap.conf') do
+    it { should be_owned_by swift_config_owner }
+    its('group') { should eq 'swift' }
     its('mode') { should eq 0640 }
     it { should exist }
   end
 end
 
 control 'check-swift-03' do
+  describe file('/etc/swift/policy.json') do
+    it { should be_owned_by swift_config_owner }
+    its('group') { should eq 'swift' }
+    its('mode') { should eq 0640 }
+    it { should exist }
+  end
+end
+
+control 'check-swift-04' do
+  describe file('/etc/swift/rootwrap.conf') do
+    it { should be_owned_by swift_config_owner }
+    its('group') { should eq 'swift' }
+    its('mode') { should eq 0640 }
+    it { should exist }
+  end
+end
+
+control 'check-swift-05' do
   describe port(6002) do
     it { should be_listening }
-    its('protocol') {should eq 'tcp'}
+    its('protocols') {should include 'tcp'}
   end
 
   describe port(6001) do
     it { should be_listening }
-    its('protocol') {should eq 'tcp'}
+    its('protocols') {should include 'tcp'}
   end
 
   describe port(6000) do
     it { should be_listening }
-    its('protocol') {should eq 'tcp'}
+    its('protocols') {should include 'tcp'}
   end
 
   describe port(873) do
     it { should be_listening }
-    its('protocol') {should eq 'tcp'}
+    its('protocols') {should include 'tcp'}
   end
 end
